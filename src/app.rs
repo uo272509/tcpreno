d395cffbf5dab36b77ac304600a63c6c62c1fd9f
Marcos Gutiérrez Alonso <30444886+margual56@users.noreply.github.com>
2023-10-30 22:21:50 +0100

    Add GUI (#1)
    * Add basic ui
    
    * Add plot
    
    * Add editing of the losses
    
    * Add web version

---
+++ src/app.rs
@@ -0,0 +1,244 @@
+use eframe::egui;
+use egui_extras::{TableBuilder, Column};
+use egui_plot::{Line, LineStyle, Legend, VLine};
+use crate::algorithm;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Algorithm {
+    Reno,
+    Tahoe,
+}
+
+impl From<String> for Algorithm {
+    fn from(s: String) -> Self {
+        if s.to_ascii_lowercase().trim().eq("reno") {
+            Algorithm::Reno
+        } else if s.to_ascii_lowercase().trim().eq("tahoe") {
+            Algorithm::Tahoe
+        } else {
+            Algorithm::Tahoe
+        }
+    }
+}
+
+impl Algorithm {
+    pub fn is_reno(&self) -> bool {
+        match self {
+            Self::Reno => true,
+            _ => false,
+        }
+    }
+}
+
+pub struct App {
+    pub cycles: usize,
+    pub threshold: u16,
+    pub window: u16,
+    pub losses: Vec<u16>,
+    pub algorithm: Algorithm,
+    pub window_size_data: Vec<[f64; 2]>,
+    pub threshold_data: Vec<[f64; 2]>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        let (data_window, data_threshold) = algorithm(
+            1,
+            8,
+            25,
+            vec![10, 14, 20],
+            true,
+        );
+
+        let window_data_zipped: Vec<[f64; 2]> = (0..data_window.len()).zip(data_window.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+        let threshold_data_zipped: Vec<[f64; 2]> = (0..data_threshold.len()).zip(data_threshold.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+
+        Self {
+            cycles: 25,
+            threshold: 8,
+            window: 1,
+            losses: vec![10, 14, 20],
+            algorithm: Algorithm::Reno,
+            window_size_data: window_data_zipped,
+            threshold_data: threshold_data_zipped,
+        }
+    }
+}
+
+impl App {
+    pub fn new(
+        cycles: usize,
+        threshold: u16,
+        window: u16,
+        losses: Vec<u16>,
+        algo: Algorithm,
+    ) -> Self {
+        let (data_window, data_threshold) = algorithm(
+            window,
+            threshold,
+            cycles,
+            losses.clone(),
+            algo.is_reno(),
+        );
+
+        let window_data_zipped: Vec<[f64; 2]> = (0..data_window.len()).zip(data_window.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+        let threshold_data_zipped: Vec<[f64; 2]> = (0..data_threshold.len()).zip(data_threshold.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+
+        Self {
+            cycles,
+            threshold,
+            window,
+            losses,
+            algorithm: algo,
+            window_size_data: window_data_zipped,
+            threshold_data: threshold_data_zipped,
+        }
+    }
+
+    fn update_data(&mut self) {
+        let (data_window, data_threshold) = algorithm(
+            self.window,
+            self.threshold,
+            self.cycles,
+            self.losses.clone(),
+            self.algorithm.is_reno(),
+        );
+
+        self.window_size_data = (0..data_window.len()).zip(data_window.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+        self.threshold_data = (0..data_threshold.len()).zip(data_threshold.iter()).map(|(x, y)| [x as f64, *y as f64]).collect();
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("Options").show(ctx, |ui| {
+            ui.heading("Options");
+
+            ui.add(
+                egui::Slider::from_get_set(5.0..=50.0, |v| {
+                    if let Some(value) = v {
+                        self.cycles = value as usize;
+
+                        self.update_data();
+
+                        value
+                    } else {
+                        self.cycles as f64
+                    }
+                })
+                .max_decimals(0)
+                .text("cycles"),
+            )
+            .on_hover_text_at_pointer("The number of cycles to simulate.");
+
+            ui.add(
+                egui::Slider::from_get_set(1.0..=50.0, |v| {
+                    if let Some(value) = v {
+                        self.threshold = value as u16;
+
+                        self.update_data();
+
+                        value
+                    } else {
+                        self.threshold as f64
+                    }
+                })
+                .max_decimals(0)
+                .text("threshold"),
+            )
+            .on_hover_text_at_pointer("The initial threshold.");
+
+            ui.add(
+                egui::Slider::from_get_set(1.0..=100.0, |v| {
+                    if let Some(value) = v {
+                        self.window = value as u16;
+
+                        self.update_data();
+
+                        value
+                    } else {
+                        self.window as f64
+                    }
+                })
+                .max_decimals(0)
+                .text("window"),
+                )
+                .on_hover_text_at_pointer("The initial window size.");
+
+
+            ui.horizontal(|ui| {
+                ui.label("Algorithm:");
+                ui.radio_value(&mut self.algorithm, Algorithm::Reno, "Reno");
+                ui.radio_value(&mut self.algorithm, Algorithm::Tahoe, "Tahoe");
+            });
+
+            ui.collapsing("Edit cycles where there are losses", |ui| {
+                TableBuilder::new(ui)
+                    .column(Column::auto().resizable(true))
+                    .column(Column::remainder())
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.heading("Cycle");
+                        });
+                        header.col(|_ui| {
+                        });
+                    })
+                .body(|mut body| {
+                    let mut update = false;
+
+                    for (i, loss) in self.losses.clone().iter().enumerate() {
+                        body.row(30.0, |mut row| {
+                            row.col(|ui| {
+                                ui.add(egui::DragValue::from_get_set(|v| {
+                                    if let Some(value) = v {
+                                        self.losses[i] = value as u16;
+                                        update = true;
+                                        value
+                                    } else {
+                                        *loss as f64
+                                    }
+                                }).speed(1.0).clamp_range(0.0..=self.cycles as f64));
+                            });
+
+                            row.col(|ui| {
+                                if ui.button("🗑").clicked() {
+                                    self.losses.remove(i);
+                                    update = true;
+                                }
+                            });
+                        });
+                    }
+
+                    body.row(30.0, |mut row| {
+                        row.col(|ui|{
+                            if ui.button("Add a loss").clicked() {
+                                self.losses.sort_unstable();
+                                self.losses.push(self.losses[self.losses.len()-1] + 1);
+                                update = true;
+                            }
+                        });
+                    });
+
+                    if update {
+                        self.update_data();
+                    }
+                });
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui_plot::Plot::new("example_plot")
+                .height(ui.available_height()/2.0)
+                .show_axes(true)
+                .legend(Legend::default())
+                .show(ui, |plot_ui| {
+                    for loss in &self.losses {
+                        plot_ui.vline(VLine::new(*loss as f64).color(egui::Color32::from_rgb(210,189,57)).style(LineStyle::dotted_dense()));
+                    }
+
+                    plot_ui.line(Line::new(self.window_size_data.clone()).name("Window size"));
+                    plot_ui.line(Line::new(self.threshold_data.clone()).name("Threshold").style(LineStyle::dashed_dense()));
+                });
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use clap::{command, Parser};
-use plotly::{
-    common::{DashType, Line, Title},
-    layout::Axis,
-    Layout, Plot, Scatter,
-};
+use eframe::egui;
+use tcpreno::App;
 
+#[cfg(not(target_arch = "wasm32"))]
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Cli {
-    #[clap(long, short, help = "Number of cycles to calculate")]
+    #[clap(
+        long,
+        short,
+        help = "Number of cycles to calculate",
+        default_value_t = 20
+    )]
     cycles: usize,
 
-    #[clap(long, short, help = "The initial threshold")]
+    #[clap(long, short, help = "The initial threshold", default_value_t = 8)]
     threshold: u16,
 
     #[clap(
@@ -21,7 +24,7 @@ struct Cli {
         value_delimiter = ',',
         help = "An array of the cycles on which a loss occurs"
     )]
-    losses: Vec<usize>,
+    losses: Vec<u16>,
 
     #[clap(
         long,
@@ -32,75 +35,63 @@ struct Cli {
     algorithm: String,
 }
 
-fn main() {
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> Result<(), eframe::Error> {
     let args = Cli::parse();
 
-    let is_reno = args.algorithm.to_ascii_lowercase().trim().eq("reno");
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1500.0, 700.0)),
+        ..Default::default()
+    };
 
-    let mut window_size = 1;
-    let mut threshold = args.threshold;
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(move |cc| {
+            //Increase the font size
+            let mut st = (*egui::Context::default().style()).clone();
+            st.override_font_id = Some(egui::FontId::monospace(14.0));
+            st.spacing.slider_width = 250.0;
+            st.spacing.button_padding = egui::Vec2::new(10.0, 5.0);
+            st.spacing.item_spacing = egui::Vec2::new(10.0, 10.0);
+            cc.egui_ctx.set_style(st);
 
-    let mut window_sizes: Vec<u16> = vec![window_size];
-    let mut thresholds: Vec<u16> = vec![threshold];
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Box::<App>::new(App::new(
+                args.cycles,
+                args.threshold,
+                1,
+                args.losses,
+                args.algorithm.into(),
+            ))
+        }),
+    )
+}
 
-    for cycle in 0..args.cycles {
-        if args.losses.contains(&cycle) {
-            threshold /= 2;
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    let web_options = eframe::WebOptions::default();
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "Canvas", // hardcode it
+                web_options,
+                Box::new(move |cc| {
+                    //Increase the font size
+                    let mut st = (*egui::Context::default().style()).clone();
+                    st.override_font_id = Some(egui::FontId::monospace(14.0));
+                    st.spacing.slider_width = 250.0;
+                    st.spacing.button_padding = egui::Vec2::new(10.0, 5.0);
+                    st.spacing.item_spacing = egui::Vec2::new(10.0, 10.0);
+                    cc.egui_ctx.set_style(st);
 
-            if is_reno {
-                window_size = threshold;
-            } else {
-                window_size = 1;
-            }
-        } else if window_size < threshold {
-            window_size *= 2;
-
-            if window_size > threshold {
-                window_size = threshold;
-            }
-        } else {
-            window_size += 1;
-            threshold = window_size;
-        }
-
-        // agregar valores de tamaño de ventana y umbral al vector correspondiente
-        window_sizes.push(window_size);
-        thresholds.push(threshold);
-    }
-
-    // Generar scatter plots de cada uno de los vectores
-    let window_size_trace =
-        Scatter::new((0..window_sizes.len()).collect(), window_sizes.clone()).name("Window size");
-    let threshold_trace = Scatter::new((0..thresholds.len()).collect(), thresholds.clone())
-        .line(Line::new().dash(DashType::DashDot))
-        .name("Threshold");
-
-    // Crear un plot
-    let layout = Layout::new()
-        .title(Title::new(&format!(
-            "{} Window Size and Threshold",
-            args.algorithm
-        )))
-        .x_axis(Axis::new().title(Title::new("Cycle")))
-        .y_axis(Axis::new().title(Title::new("Window Size / Threshold")));
-
-    let mut plot = Plot::new();
-    plot.add_trace(window_size_trace);
-    plot.add_trace(threshold_trace);
-    plot.set_layout(layout);
-
-    // Y mostrarlo
-    plot.show();
-
-    // Y además generar el csv
-    println!("Cycle;Window size;Threshold;Packet losses");
-    for (i, (window, threshold)) in window_sizes.iter().zip(thresholds.iter()).enumerate() {
-        println!(
-            "{};{};{};{}",
-            i,
-            window,
-            threshold,
-            if args.losses.contains(&i) { "Loss" } else { "" }
-        );
-    }
+                    // This gives us image support:
+                    egui_extras::install_image_loaders(&cc.egui_ctx);
+                    Box::<App>::default()
+                }),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
