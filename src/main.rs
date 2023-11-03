03ecfa6403cd464ce1697ab30bbff4fcd509054c
Marcos Gutiérrez Alonso <marcos56@mailbox.org>
2023-11-03 10:43:43 +0100

    Disable console on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use clap::{command, Parser};
 use eframe::egui;
 use tcpreno::App;
